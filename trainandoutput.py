7713b6dd30cace1bb96834b798346561c4f58fa9
steve <551619855@qq.com>
2017-10-19 08:52:10 +0800

    !

--- trainandoutput.py
+++ trainandoutput.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Created by steve @ 17-10-19 上午8:40
+# Created by steve @ 17-10-19 上午8:51
 '''
                    _ooOoo_ 
                   o8888888o 
@@ -22,40 +22,3 @@
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
          佛祖保佑       永无BUG 
 '''
-
-import scipy.io as sio
-import matplotlib.pyplot as plt
-
-import numpy as np
-
-from sklearn.model_selection import train_test_split
-
-
-import torch
-from torch import
-
-if __name__ == '__main__':
-    dir_name = '/home/steve/Data/Shad/'
-
-    # x_file = sio.loadmat(dir_name+'Mill_500_25.mat')
-    x_file = sio.loadmat(dir_name+ 'Sond.mat')
-    print(x_file)
-
-    # x = x_file['X']
-    # y = x_file['Y']
-    x = x_file['X_temp']
-    y = x_file['Y_temp']
-
-    x = x.transpose()
-    y = y.transpose()
-
-
-    print(x.shape)
-    print(y.shape)
-
-
-    x_train,x_valid,y_train,y_valid = train_test_split(x,y,test_size=0.2)
-
-
-    print(x_train.shape,x_valid.shape,y_train.shape,y_valid.shape)
-
